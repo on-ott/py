0e48b57c774e5c5551d69c6632d88db0d980071b
on-ott <kenong0136@gmail.com>
2025-11-07 16:16:52 +0800

    Create index.js

---
+++ project/index.js
@@ -0,0 +1,15 @@
+export default {
+  async fetch(request) {
+    const ua = request.headers.get("User-Agent") || "";
+
+    if (!/OTTPlayer|SmartIPTV|SSIPTV/i.test(ua)) {
+      return new Response("403 Forbidden - OTT Only", { status: 403 });
+    }
+
+    const target = "https://yourusername.github.io"; // 源站地址
+    const url = new URL(request.url);
+    const newUrl = target + url.pathname + url.search;
+
+    return fetch(newUrl, request);
+  },
+};
